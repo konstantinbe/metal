ec98aec9f6a387e4a8bce72cac566a655e8a6d10
Konstantin Bender <me@konstantinbender.com>
2012-09-20 20:43:25 +0200

    Add commands MLWithArray() and MLWithCapacity()

--- source/commands.h
+++ source/commands.h
@@ -24,8 +24,12 @@
 
 #define MLCreate(self) MLSend(self, IS("create"), null, null)
 #define MLNew(self) MLSend(self, IS("new"), null, null)
+
 #define MLNewWithArray(self, array) MLSend(self, IS("new-with-array*"), IA(array), null)
 #define MLNewWithCapacity(self, capacity) MLSend(self, IS("new-with-capacity*"), IA(capacity), null)
+
+#define MLWithArray(self, array) MLSend(self, IS("with-array*"), IA(array), null)
+#define MLWithCapacity(self, capacity) MLSend(self, IS("with-capacity*"), IA(capacity), null)
 
 #define MLName(self) MLSend(self, IS("name"), null, null)
 #define MLSuperclass(self) MLSend(self, IS("superclass"), null, null)
