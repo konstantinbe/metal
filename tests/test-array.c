f0422a27994d2dfb0542c84fe24a5391a6f385ea
Konstantin Bender <me@konstantinbender.com>
2012-12-04 19:36:25 +0100

    Improve test descriptions for -insert-many*before*

--- tests/test-array.c
+++ tests/test-array.c
@@ -524,10 +524,10 @@ void MLTestMutableArrayInsertBefore() {
     MLInsertBefore(array2, N(4), N(9));
     MLInsertBefore(array3, N(6), N(9));
     MLInsertBefore(array4, N(5), N(4));
-    MLAssertEquals(array1, IA(N(4), N(5), N(6)), "MutableArray -insert*before* inserts an object before an passed in object");
+    MLAssertEquals(array1, IA(N(4), N(5), N(6)), "MutableArray -insert*before* inserts an object before a passed in object");
     MLAssertEquals(array2, IA(N(4), N(6)), "MutableArray -insert*before* inserts an object at the beginning if the before object is not contained");
     MLAssertEquals(array3, IA(N(6)), "MutableArray -insert*before* inserts an object into an empty array ignoring the passed in before object");
-    MLAssertEquals(array4, IA(N(5), N(4), N(5), N(6)), "MutableArray -insert*before* inserts an object at the beginning if the before an object is the first in the array");
+    MLAssertEquals(array4, IA(N(5), N(4), N(5), N(6)), "MutableArray -insert*before* inserts an object at the beginning if the before object is the first in the array");
 }
 
 
